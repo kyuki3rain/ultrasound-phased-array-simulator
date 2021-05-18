3adada0fc72cf4ae84c24f0db108e07085620bf4
kyuki3rain <kyuki3rain@gmail.com>
2021-05-18 14:51:12 +0900

    コメント追加と微修正

--- test3.m
+++ test3.m
@@ -4,13 +4,14 @@ focus_x = 50;
 height = 1000;
 focus_y = 500;
 N = 50;
-w = width / N;
-f = 100000;
+f = 1000;
 s = 340*1000;
+
+w = width / N;
 lambda = s / f;
 
 Field = zeros(width, height);
-Waves = initialize(N);
+Waves = initialize(N, focus_x, focus_y, lambda, w);
 
 for i = 1:N
     for x = 1:width
@@ -27,14 +28,19 @@ I = mat2gray(abs(Field));
 figure
 imshow(I)
 
+figure
+plot(abs(Field(:,500)))
+
 function distance = calc_distance(x, y, i, w)
     distance = sqrt((x - i * w)^2 + y ^ 2);
 end
 
-function Waves = initialize(N)
+function Waves = initialize(N, width, height, lambda, w)
     Waves = zeros(N);
     for i = 1:N
-        Waves(i) = 1 + 0i;
+        distance = calc_distance(width, height, i, w);
+        angle = calc_angle(distance, lambda);
+        Waves(i) = complex(cos(-angle), sin(-angle));
     end
 end
 
